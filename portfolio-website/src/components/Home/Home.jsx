7ae76a6677b85dfb749d2c0858952b1ecd089550
Satvik Vansh (BIT) <satvikvansh@gmail.com>
2025-07-23 01:58:27 +0530

    initialized ShadCN

--- portfolio-website/src/components/Home/Home.jsx
+++ portfolio-website/src/components/Home/Home.jsx
@@ -86,9 +86,9 @@ function Home() {
 
 
       <div id="projects">
-        <div className="flex justify-center items-center content-center h-screen flex-3 bg-[#180133]">
+        <div className=" h-screen flex-3 bg-[#180133]">
           {/* header */}
-          <div className='mt-20 mb-10 flex flex-col justify-center'>
+          <div className='mt-20 mb-10 flex flex-col justify-center items-center'>
             <div className='flex justify-center'>
               <div className=' bg-purple-500 w-3 rounded-sm drop-shadow-lg drop-shadow-purple-400/70'></div>
               <h1 className='px-10 bg-linear-to-r from-purple-800/50 to-pink-300/0 text-7xl font-bold text-purple-500'>Projects<span className="text-yellow-300">.</span></h1>
@@ -96,6 +96,15 @@ function Home() {
             <div className='flex justify-center m-5'>
               <p className="bg-linear-[160deg] from-gray-50 to-gray-400 inline-block text-transparent bg-clip-text text-xl text-l font-light">My projects based on <span className='font-medium'>Web Development</span><span className="text-yellow-300">.</span></p>
             </div>
+          </div>
+
+          {/* List */}
+          <div className='flex flex-row justify-around mx-30'>
+            {/* <div className='w-90 h-100 bg-blue-600 rounded-2xl'>
+
+            </div>
+            <div className='w-90 h-100 bg-blue-600 rounded-2xl'></div>
+            <div className='w-90 h-100 bg-blue-600 rounded-2xl'></div> */}
           </div>
           
         </div>
